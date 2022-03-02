2dcec2fe7447259791f29b5507a05f8420ea5495
santocaridi99 <santocaridi99@gmail.com>
2022-03-02 17:48:08 +0100

    aggiunto file css per modificare pagina iniziale

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,14 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hello Laravel</title>
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
-    <h1>Hello laravel</h1>
-    <ul>
+   <div class="navbar">
+     <h1>Hello laravel</h1>
+     <ul>
         @foreach ($navbar as $value)
         {{-- stampo link e testo del data --}}
         <li><a href="{{$value["link"]}}">{{$value["text"]}}</a></li>
         @endforeach
-    </ul>
+     </ul>
+    </div>
 </body>
 </html>
