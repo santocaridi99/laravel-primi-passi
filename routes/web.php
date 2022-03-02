923f3272f893f25bb02aa17db2589a376936ff2f
santocaridi99 <santocaridi99@gmail.com>
2022-03-02 15:50:52 +0100

    Creato un semplice Helloworld e passato dei dati provenienti da un array

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $data = [
+        "navbar" => [
+            "Chi siamo",
+            "Contatti",
+            "Network",
+            "Shop"
+        ]
+    ];
+    return view('home', $data);
 });
